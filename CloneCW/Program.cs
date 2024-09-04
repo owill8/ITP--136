074cb8d5adef95569c147c614ec3b928cb57539b
Owen Williams <owenc.williams@comcast.net>
2024-09-04 19:45:21 -0400

    commit 2

--- CloneCW/Program.cs
+++ CloneCW/Program.cs
@@ -4,13 +4,11 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            //this is a comment
-            
-            /* this is paragraph format
-             * 
-             * 
-             */
+            Console.WriteLine("Who is being seen today");
+            string patientnName = Console.ReadLine();//input
+
+            Console.WriteLine("welcome: " + patientnName);//output
+
            
         }
     }
