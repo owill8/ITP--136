2d59097d809872b7fb5316a2aa72c72a91f7833e
Owen Williams <owenc.williams@comcast.net>
2024-09-18 20:26:30 -0400

    project 4

---
+++ week 4 project2/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace week_4_project2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("Welcome to doctor visit calculator");
+            Console.WriteLine("----------------------------------");
+
+            Console.WriteLine("Enter in 1-Doctor Sick Apointment or 2-Doctor check up");
+           int option = Convert.ToInt32(Console.ReadLine());
+
+            switch (option)
+            {
+                case 1:
+
+                    int base_visit_child1 = 50;
+                    int adult1 = 75;
+                    int total, total2;
+
+                    Console.WriteLine("are you a child-1 or a adult-2");
+                    int choice = Convert.ToInt32(Console.ReadLine());
+
+                    if (choice == 1 || choice == 2)
+                    {
+                        Console.WriteLine("Child price is: {0}", base_visit_child1.ToString("C"));
+                        Console.WriteLine("Adult price is: {0}", adult1.ToString("C"));
+                    }
+                    else {
+                        Console.WriteLine("Enter in 1 or 2 please");
+                    }
+                    Console.WriteLine("Did you have labs done? Y/N");
+                    char response = Convert.ToChar(Console.ReadLine());
+
+                    if (response == 'Y' || response == 'y')
+                    {
+                        total = base_visit_child1 + 25;
+                        total2 = adult1 + 25;
+                        Console.WriteLine("Your total price for a Adult is: {0}",total2.ToString("C"));
+                        Console.WriteLine("Your total price for a Child is: {0}",total.ToString("C"));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Your total price for a Adult is: {0}", adult1.ToString("C"));
+                        Console.WriteLine("Your total price for a Child is: {0}", base_visit_child1.ToString("C"));
+                    }
+                    break;
+                case 2:
+                    int base_visit_child = 75;
+                    int adult = 100;
+                    Console.WriteLine("Are you a child-1 or a adult-2");
+                    int choice2 = Convert.ToInt32(Console.ReadLine());
+
+                    if (choice2 == 1)
+                    {
+                        Console.WriteLine("Your total price for a child is: {0}", base_visit_child.ToString("C"));
+                    }
+                    else if (choice2 == 2)
+                    {
+                        Console.WriteLine("Your total price for a Adult is: {0}",adult.ToString("C"));
+                    }
+                    else
+                    {
+                        Console.WriteLine("Enter in 1 or 2 please");
+                    }
+                    break;
+                       
+                 
+           
+                            
+                            
+              }
+
+
+            Console.ReadKey();
+        
+        }
+    }
+}
---
+++ week 4/Program.cs
@@ -0,0 +1,102 @@
+﻿using static System.Console;
+namespace week_4
+
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int first = 5;
+            int second = 6;
+
+            if (first == second)
+            {
+                WriteLine("The numbers are equal");
+                
+            }
+            else
+            {
+                WriteLine("The numbers are not equal");
+                if (first > second)
+                {
+                    WriteLine("First number is greater than second");
+                }
+                else
+                {
+                    WriteLine("Second is greater than first");
+                }
+            }
+
+            char ch = 'x';
+            if (ch == 'a' || ch == 'A')
+            {
+                WriteLine("Vowel");
+            }
+            else if (ch == 'e' || ch == 'E')
+            {
+                WriteLine("Vowel");
+            }
+            else if (ch == 'i' || ch == 'I')
+            {
+                WriteLine("Vowel");
+            }
+            else if (ch == 'o' || ch == 'O')
+            {
+                WriteLine("Vowel");
+            }
+            else if (ch == 'u' || ch == 'U')
+            {
+                WriteLine("Vowel");
+            }
+            else
+            {
+                WriteLine("Constant");
+            }
+
+
+            int option, a, b, total;
+            WriteLine("Enter in first number");
+            a = Convert.ToInt32(Console.ReadLine());
+
+            WriteLine("Enter in your second number");
+            b= Convert.ToInt32(Console.ReadLine());
+
+            WriteLine("1- Add, 2-subtract, 3- multiply, 4-divide");
+            option = Convert.ToInt32(Console.ReadLine());
+
+
+            switch (option)
+            {
+                case 1:
+                    total = a + b;
+                    WriteLine("the numbers added are {0},", total);
+                    break;
+                case 2:
+                    total = a - b;
+                    WriteLine("the numbersubtracted are {0},", total);
+                    break;
+                case 3:
+                    total = a * b;
+                    WriteLine("The numbers times are {0}", total);
+                    break;
+                case 4:
+                    total = a / b;
+                    WriteLine("The numbers divided are {0}", total);
+                    break;
+
+                default:
+                    WriteLine("Enter in bad number");
+                    break;
+            }
+
+
+        
+        
+        
+        
+        
+        
+        
+        }
+    }
+}
