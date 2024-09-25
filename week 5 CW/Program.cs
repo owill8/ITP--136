69292b4bc9e33ca643f0cdfb53106410e993f700
Owen Williams <owenc.williams@comcast.net>
2024-09-25 19:18:08 -0400

    new commit

---
+++ week 5 CW/Program.cs
@@ -0,0 +1,52 @@
+﻿namespace week_5_CW
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b;
+
+            a = 0;
+            b = 5;
+
+            // //while (a < b)
+            // {
+            //     Console.WriteLine(a);
+
+
+            //     a++;
+            //// }
+            ///
+
+            //1st part inistial value 
+            //2nd part is the condition statment 
+            //3rd part is the incrementer
+
+            //variables 
+            int total;
+
+            Console.WriteLine("Welcome to my calculator");
+            
+            
+            
+            
+            
+            
+            for (int k = 0; k < 3; k++)
+
+            {
+                Console.WriteLine("Enter in your first number");
+                a = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter in your second number");
+                b = Convert.ToInt32(Console.ReadLine());
+
+                total = a + b;
+
+                Console.WriteLine("your total is: " + total);
+            }
+            Console.WriteLine("Thank you for using the calculator");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ week 5 project/Program.cs
@@ -0,0 +1,47 @@
+﻿
+
+namespace week_5_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+           
+            int total;
+
+
+            Console.WriteLine("*.*.*.*.*.*.*.*.*.*.*.*.*.*.*");
+            Console.WriteLine("Welcome to sports calculator");
+            Console.WriteLine("*.*.*.*.*.*.*.*.*.*.*.*.*.*.*");
+
+            
+
+
+
+            for (int i = 0; i < 5; i++)
+            {
+                Console.WriteLine("Enter in your amount of goals scored:");
+                int goals = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine("Enter in your amount of assists");
+                int assists= Convert.ToInt32(Console.ReadLine());
+
+
+                total = goals + assists;
+
+                Console.WriteLine("Your total points in your game are {0}", total);
+
+
+            }
+            Console.WriteLine("thanks for using my sports calculator");
+            Console.ReadLine();
+
+
+
+
+
+        }
+    }
+}
