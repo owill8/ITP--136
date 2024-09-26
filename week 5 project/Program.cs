ede0a4000c088122c4b56f430e91094f7c8f38d3
Owen Williams <owenc.williams@comcast.net>
2024-09-26 14:34:30 -0400

    commit 9/26/2024

--- week 5 project/Program.cs
+++ week 5 project/Program.cs
@@ -9,33 +9,53 @@ namespace week_5_project
 
 
            
-            int total;
+            int total_1;
+            int total_2;
+            int goals = 0;
+            int assists = 0;
+            
 
 
             Console.WriteLine("*.*.*.*.*.*.*.*.*.*.*.*.*.*.*\n");
             Console.WriteLine("Welcome to sports calculator\n");
             Console.WriteLine("*.*.*.*.*.*.*.*.*.*.*.*.*.*.*\n");
 
-            
+            Console.WriteLine("Enter in the amount of games you want to caclulate: ");
+            int counter = Convert.ToInt32(Console.ReadLine());
 
 
 
-            for (int i = 0; i < 5; i++)
+            for (int i = 0; i < counter; i++)
             {
+
+
+               
+                
+                
                 Console.WriteLine("Enter in your amount of goals scored: ");
-                int goals = Convert.ToInt32(Console.ReadLine());
+                 goals = Convert.ToInt32(Console.ReadLine());
 
                 Console.WriteLine("Enter in your amount of assists");
-                int assists= Convert.ToInt32(Console.ReadLine());
+                 assists= Convert.ToInt32(Console.ReadLine());
 
 
-                total = goals + assists;
-
-                Console.WriteLine("Your total points in your game are {0}", total);
+               
 
 
             }
-            Console.WriteLine("thanks for using my sports calculator");
+
+
+
+            total_1 = (goals + assists) * counter;
+            total_2 = goals + assists;
+            double average = total_2 / counter;
+
+
+            Console.WriteLine("Your total points = {0}",  total_1 + " Points\n ");
+            Console.WriteLine("Your average points per game = {0}\n", average);
+            Console.WriteLine("Thanks for using my sports calculator!!\n");
+            Console.WriteLine("\t\t PRESS ENTER TO CLOSE!");
+           
             Console.ReadLine();
 
 
