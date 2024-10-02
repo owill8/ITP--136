357c99abb4caa65771d680e4750c5da2dcf3d320
Owen Williams <owenc.williams@comcast.net>
2024-10-02 19:27:29 -0400

    project number 6

---
+++ CW-week_6/Program.cs
@@ -0,0 +1,62 @@
+﻿namespace CW_week_6
+{
+    internal class Program
+    {
+        static void Main(string[] args)//this is a method 
+        {
+            //methods!!,Console.WriteLine() is a method itself;
+            //calling method 
+
+            int a, b;
+
+            WelcomeMessage();
+            for (int i = 0; i < 3; i++)//3 problems for adding because of for loop
+            {
+                a = askNumber();
+                b = askNumber();
+
+                addNumber(a, b);
+            }
+            Console.WriteLine("This ends the program");
+
+            Console.ReadLine();
+
+           
+        }
+        static void WelcomeMessage()
+        {
+            Console.WriteLine("Welcome to my calculator");
+
+            Console.ReadKey();
+        }
+        static int askNumber()//return method 
+        {
+            int x;
+            Console.WriteLine("Please enter in a number");
+            x = Convert.ToInt32(Console.ReadLine());
+
+            return x;
+        }
+
+        static void addNumber(int num1, int num2)
+        {
+            int total = num1 + num2;
+            Console.WriteLine("Your total is {0}", total);
+
+           
+        }
+        static int addNumber2(int num1, int num2)
+        {
+            int total = num1 + num2;
+
+            return total;
+        }
+
+    
+    
+    
+    
+    
+    
+    }
+}
---
+++ week 6 project/Program.cs
@@ -0,0 +1,68 @@
+﻿namespace week_6_project
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            Console.WriteLine("How many method examples do you want?");
+            int count = Convert.ToInt32(Console.ReadLine());
+            
+            
+            
+            
+            
+            for (int i = 0; i < count; i++)
+            {
+                WelcomeStatement();
+                int secretNumber = 8;
+                MagicNumber(secretNumber);
+
+                FindArea();
+
+                double taxRate = LocalTaxRate();//taxRate variable
+
+
+                Console.WriteLine("Your tax rate is " + taxRate);
+            }
+        }
+        static void WelcomeStatement()
+        {
+            Console.WriteLine("*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*\n");
+            Console.WriteLine("   Welcome to My Method Examples    \n");
+            Console.WriteLine("*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*.*\n");
+        }
+        static void MagicNumber(int secretNumber)
+        {
+            Console.WriteLine("Your Magic Number is " + secretNumber);
+        }
+        static void FindArea()
+        {
+            Console.WriteLine("Enter in your length: ");
+             int length = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Enter in your width: ");
+             int width = Convert.ToInt32(Console.ReadLine());
+
+            double result = length * width;
+
+            Console.WriteLine("Your area is {0}" , result);
+
+            Console.ReadKey();
+        }
+
+        static double LocalTaxRate()
+        {
+            Console.WriteLine("Please enter your tax rate");
+            double taxRate = Convert.ToDouble(Console.ReadLine());
+
+            return taxRate;
+        }
+
+       
+
+
+    }
+}
