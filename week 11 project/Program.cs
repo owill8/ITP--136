2438ccd90b7877b82e3c86948efab4d449ba062c
Owen Williams <owenc.williams@comcast.net>
2024-11-09 11:56:39 -0500

    changes

--- week 11 project/Program.cs
+++ week 11 project/Program.cs
@@ -11,78 +11,80 @@
             decimal a, b, result;
             int option;
 
+           
+            
+                Console.WriteLine("\nEnter in number 1: ");
+                a = Convert.ToDecimal(Console.ReadLine());
 
-            Console.WriteLine("Enter in number 1: ");
-            a = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Enter in number 2: ");
+                b = Convert.ToDecimal(Console.ReadLine());
 
-            Console.WriteLine("Enter in number 2: ");
-            b = Convert.ToDecimal(Console.ReadLine());
+                Console.WriteLine("Choose one of the following");
+                Console.WriteLine("1.Add");
+                Console.WriteLine("2.Multiply");
+                Console.WriteLine("3.Subtraction");
+                Console.WriteLine("4.Division");
+                option = Convert.ToInt32(Console.ReadLine());
 
-            Console.WriteLine("Choose one of the following");
-            Console.WriteLine("1.Add");
-            Console.WriteLine("2.Subtract");
-            Console.WriteLine("3.Divide");
-            Console.WriteLine("4.Multiply");
-            option = Convert.ToInt32(Console.ReadLine());
+                if (option == 1)
+                {
+                    try
+                    {
+                        result = a + b;
+                        Console.WriteLine("Your result is: " + result);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("Exception" + ex);
+                    }
 
-            if (option == 1)
-            {
-                try
-                {
-                    result = a + b;
-                    Console.WriteLine("Your result is: " + result);
                 }
-                catch (Exception ex)
+                else if (option == 2)
                 {
-                    Console.WriteLine("Exception" + ex);
-                }
-               
-             }
-            else if (option == 2) 
-            {
 
-                try
-                {
-                    result = a - b;
-                    Console.WriteLine("Your result is: " + result);
+                    try
+                    {
+                        result = a * b;
+                        Console.WriteLine("Your result is: " + result);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("ERROR CAUGHT");
+                    }
                 }
-                catch (Exception ex)
+                else if (option == 3)
                 {
-                    Console.WriteLine("ERROR CAUGHT");
-                }
-            }
-            else if (option == 3)
-            {
 
-                try
-                {
-                    result = a / b;
-                    Console.WriteLine("Your result is: " + result);
+                    try
+                    {
+                        result = a - b;
+                        Console.WriteLine("Your result is: " + result);
+                    }
+                    catch (Exception ex)
+                    {
+                        Console.WriteLine("ERROR CAUGHT" + ex);
+                    }
                 }
-                catch (DivideByZeroException ex)
+                else if (option == 4)
                 {
-                    Console.WriteLine("ERROR CAUGHT" + ex);
-                }
-            }
-            else if (option == 4)
-            {
 
-                try
-                {
-                    result = a * b;
-                    Console.WriteLine("Your result is: " + result);
+                    try
+                    {
+                        result = a / b;
+                        Console.WriteLine("Your result is: " + result);
+                    }
+                    catch (DivideByZeroException ex)
+                    {
+                        Console.WriteLine("()((_()_(())*ERROR CAUGHT*)(*&()*)*(&)" + ex);
+                    }
                 }
-                catch (Exception ex)
+                else
                 {
-                    Console.WriteLine("()((_()_(())*ERROR CAUGHT*)(*&()*)*(&)" + ex);
+                    Console.WriteLine("Enter in a number 1-4 please");
                 }
-            }
-            else
-            {
-                Console.WriteLine("Enter in a number 1-4 please");
-            }
 
-            Console.ReadKey();
+                Console.ReadKey();
+            
 
         }
     }
